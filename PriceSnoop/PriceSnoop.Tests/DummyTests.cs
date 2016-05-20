80be5813430f368968636d75ba1cef0297f43587
Eadred <eadred.birchenough@zuhlke.com>
2016-05-20 12:32:33 +0100

    Added test project

---
+++ PriceSnoop/PriceSnoop.Tests/DummyTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using PriceSnoop;
+using Microsoft.AspNet.Mvc;
+
+namespace PriceSnoop.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
---
+++ PriceSnoop/PriceSnoop.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PriceSnoop.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zuehlke Technology Group")]
+[assembly: AssemblyProduct("PriceSnoop.Tests")]
+[assembly: AssemblyCopyright("Copyright © Zuehlke Technology Group 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa620a27-f029-495e-b91e-a7e077d93d8a")]
