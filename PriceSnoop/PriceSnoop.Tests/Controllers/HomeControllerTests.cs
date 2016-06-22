b3e6afe46046ae910a91ede522aca25f31a9a29c
Eadred <eadred.birchenough@zuhlke.com>
2016-06-22 13:47:17 +0100

    Added a test that references code in the main application project

---
+++ PriceSnoop/PriceSnoop.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using PriceSnoop;
+using PriceSnoop.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using PriceSnoop.Models;
+using System.Net;
+
+namespace PriceSnoop.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexViewModel_ShouldBeProductList()
+        {
+            var target = new HomeController();
+            var res = target.Index() as ViewResult;
+            Assert.Equal(typeof(List<Product>), res.Model.GetType());
+        }
+    }
+}
