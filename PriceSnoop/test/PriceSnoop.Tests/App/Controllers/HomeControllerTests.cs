159bc628e88b33bf44cfbeae0867845f8366c492
Eadred <eadred.birchenough@zuhlke.com>
2016-07-01 16:33:11 +0100

    Fixed HomeController test

--- PriceSnoop/test/PriceSnoop.Tests/App/Controllers/HomeControllerTests.cs
+++ PriceSnoop/test/PriceSnoop.Tests/App/Controllers/HomeControllerTests.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using PriceSnoop;
 using PriceSnoop.Controllers;
 using Microsoft.AspNetCore.Mvc;
-using PriceSnoop.Shared.Models;
+using vm = PriceSnoop.Shared.Models;
 using System.Net;
 using NUnit.Framework;
+using Moq;
+using PriceSnoop.Services;
+using mod = EbayClientHack.DTO.KeywordSearch;
 
 namespace PriceSnoop.Tests.App.Controllers
 {
@@ -15,11 +18,22 @@ namespace PriceSnoop.Tests.App.Controllers
     public class HomeControllerTests
     {
         [Test]
-        public void IndexViewModel_ShouldBeProductList()
+        public void IndexViewModel_ShouldReturnProductsBasedOnApiResults()
         {
-            var target = new HomeController(null);
+            var titles = new[] { "Title A", "Title B" };
+            var apiProds = titles.Select(t => new mod.Product() { Title = t }).ToArray();
+
+            var prodSearch = new Mock<IProductSearch>();
+            prodSearch
+                .Setup(ps => ps.Search(It.IsAny<string>()))
+                .Returns(apiProds);
+
+            var target = new HomeController(prodSearch.Object);
+
             var res = target.Index() as ViewResult;
-            Assert.AreEqual(typeof(List<Product>), res.Model.GetType());
+            var actualProds = res.Model as List<vm.Product>;
+
+            CollectionAssert.AreEquivalent(titles, actualProds.Select(p => p.Name).ToArray());
         }
     }
 }
