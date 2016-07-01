7f5f86227cc089a7b5751ca8a007f6d2060af2bc
Eadred <eadred.birchenough@zuhlke.com>
2016-07-01 16:00:39 +0100

    Added integration test for product search

---
+++ PriceSnoop/test/PriceSnoop.IntegrationTests/Services/Ebay/EbayApiIntegrationTests.cs
@@ -0,0 +1,28 @@
+﻿
+using NUnit.Framework;
+using PriceSnoop.Services.Ebay;
+using System;
+using System.Linq;
+
+namespace PriceSnoop.IntegrationTests.Services.Ebay
+{
+    [TestFixture]
+    public class EbayApiIntegrationTests
+    {
+        private EbayProductSearch target;
+
+        [SetUp]
+        public void Setup()
+        {
+            string ebayApiKey = Environment.GetEnvironmentVariable("EbaySettings:AppId");
+            Console.WriteLine($"ApiKey= {ebayApiKey}");
+            target = new EbayProductSearch(new EbayApi(), ebayApiKey);
+        }
+
+        [Test]
+        public void ShouldReturnProductsForValidKeyword()
+        {
+            Assert.IsTrue(target.Search("Harry Potter").Count() > 0);
+        }
+    }
+}
